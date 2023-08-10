889b61d99d864dbebdcad9e9cf099f7ea466aea6
beyzaoozdemir <44787320+beyzaoozdemir@users.noreply.github.com>
2023-08-10 14:05:12 +0300

    Create AppTest.java

---
+++ src/test/java/com/deneme/app/AppTest.java
@@ -0,0 +1,12 @@
+package com.deneme.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest{
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
